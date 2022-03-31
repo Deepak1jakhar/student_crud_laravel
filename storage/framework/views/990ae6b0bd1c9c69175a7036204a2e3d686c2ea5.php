2634b7c350cd2ee94240f78c954c03c780ea65c4
Deepak1jakhar <97865577+Deepak1jakhar@users.noreply.github.com>
2022-03-31 18:05:00 +0530

    Add files via upload

---
+++ storage/framework/views/990ae6b0bd1c9c69175a7036204a2e3d686c2ea5.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Student Crud System</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+  
+<div class="container">
+    <?php echo $__env->yieldContent('content'); ?>
+</div>
+  
+</body>
+</html><?php /**PATH C:\Users\Deepak\Desktop\projects\crudapplication\resources\views/students/layout.blade.php ENDPATH**/ ?>
